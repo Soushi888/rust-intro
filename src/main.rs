cf7af8b515322ec32de6f934e2f8aacc74113e97
Soushi888 <Sacha.pignot@protonmail.com>
2021-05-25 01:45:38 -0400

    Write arguments in kv.db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::env::args;
+use std::env::Args;
+use std::iter::Skip;
+
+fn main() -> std::io::Result<()> {
+    let mut arguments: Skip<Args> = args().skip(1);
+    let key: String = arguments.next().expect("Key was not there");
+    let value: String = arguments.next().expect("Value was not there");
+
+    println!("The key is '{}' and the value is '{}'", key, value);
+
+    let contents: String = format!("{}\t{}\n", key, value);
+    fs::write("kv.db", contents)
 }
