6983d8d4a714b29a18b9bdd700ad140c2f6f2889
Soushi888 <Sacha.pignot@protonmail.com>
2021-05-25 23:43:40 -0400

    insert method

--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,33 @@ fn main() {
 
     println!("The key is '{}' and the value is '{}'", key, value);
 
-    let contents: String = format!("{}\t{}\n", key, value);
-    fs::write("kv.db", contents).unwrap();
+    let mut database = Database::new().expect("Database::new() failed");
+    database.insert(key.clone(), value.clone());
+    database.insert(key.to_uppercase(), value.to_uppercase());
+    database.insert(format!("{}2", key), format!("{}2", value));
+    println!("{:?}", database.1);
 
-    let database = Database::new().expect("Database::new() failed");
-}
 
-struct Database(PathBuf, HashMap<String, String>);
 
-impl Database {
-    fn new() -> Result<Database, Error> {
-        let mut path: PathBuf = PathBuf::from(r"C:\Users\Catherine\Documents\Projects\rust-intro\kv.db");
-        let mut map = HashMap::new();
-        let contents = fs::read_to_string("kv.db")?;
-        for line in contents.lines() {
-            let (key, value) = line.split_once('\t').expect("Corrupt database");
-            map.insert(key.to_owned(), value.to_owned());
+    struct Database(PathBuf, HashMap<String, String>);
+
+    impl Database {
+        fn new() -> Result<Database, Error> {
+            let path: PathBuf = PathBuf::from(r"C:\Users\Catherine\Documents\Projects\rust-intro\kv.db");
+            let mut map = HashMap::new();
+            let contents = fs::read_to_string("kv.db")?;
+            for line in contents.lines() {
+                let (key, value) = line.split_once('\t').expect("Corrupt database");
+                map.insert(key.to_owned(), value.to_owned());
+            }
+
+            Ok(Database(path, map))
         }
 
-        Ok(Database(path, map))
+        fn insert(&mut self, key: String, value: String) {
+            self.1.insert(key, value);
+        }
+
+        // fn delete(self) {}
     }
 }
