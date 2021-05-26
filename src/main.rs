42ccbe749f9e4d28ad4f6d6c681bcd8b39669236
Soushi888 <Sacha.pignot@protonmail.com>
2021-05-26 00:48:13 -0400

    insert method

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::env::args;
 use std::env::Args;
 use std::fs;
-use std::io::Error;
+use std::io::Result;
 use std::iter::Skip;
 use std::path::PathBuf;
 
@@ -11,35 +11,39 @@ fn main() {
     let key: String = arguments.next().expect("Key was not there");
     let value: String = arguments.next().expect("Value was not there");
 
-    println!("The key is '{}' and the value is '{}'", key, value);
+    println!("The new key is '{}' and the new value is '{}'", key, value);
 
     let mut database = Database::new().expect("Database::new() failed");
     database.insert(key.clone(), value.clone());
-    database.insert(key.to_uppercase(), value.to_uppercase());
-    database.insert(format!("{}2", key), format!("{}2", value));
-    println!("{:?}", database.1);
+    database.flush();
+}
 
 
+struct Database(PathBuf, HashMap<String, String>);
 
-    struct Database(PathBuf, HashMap<String, String>);
-
-    impl Database {
-        fn new() -> Result<Database, Error> {
-            let path: PathBuf = PathBuf::from(r"C:\Users\Catherine\Documents\Projects\rust-intro\kv.db");
-            let mut map = HashMap::new();
-            let contents = fs::read_to_string("kv.db")?;
-            for line in contents.lines() {
-                let (key, value) = line.split_once('\t').expect("Corrupt database");
-                map.insert(key.to_owned(), value.to_owned());
-            }
-
-            Ok(Database(path, map))
+impl Database {
+    fn new() -> Result<Database> {
+        let path: PathBuf = PathBuf::from(r"C:\Users\Catherine\Documents\Projects\rust-intro\kv.db");
+        let mut map = HashMap::new();
+        let contents = fs::read_to_string("kv.db")?;
+        for line in contents.lines() {
+            let (key, value) = line.split_once('\t').expect("Corrupt database");
+            map.insert(key.to_owned(), value.to_owned());
         }
 
-        fn insert(&mut self, key: String, value: String) {
-            self.1.insert(key, value);
-        }
+        Ok(Database(path, map))
+    }
 
-        // fn delete(self) {}
+    fn insert(&mut self, key: String, value: String) {
+        self.1.insert(key, value);
+    }
+
+    fn flush(self) -> Result<()> {
+        let mut contents = String::new();
+        for pairs in self.1 {
+            let kvpair = format!("{}\t{}\n", pairs.0, pairs.1);
+            contents.push_str(&kvpair);
+        }
+        fs::write("kv.db", contents)
     }
 }
