4da3786d03fe36e3357a4a325c1927888123e13b
Soushi888 <Sacha.pignot@protonmail.com>
2021-05-25 20:56:08 -0400

    add PathBuff + to_sting() -> to_owned

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::env::Args;
 use std::fs;
 use std::io::Error;
 use std::iter::Skip;
+use std::path::PathBuf;
 
 fn main() {
     let mut arguments: Skip<Args> = args().skip(1);
@@ -15,22 +16,21 @@ fn main() {
     let contents: String = format!("{}\t{}\n", key, value);
     fs::write("kv.db", contents).unwrap();
 
-    let database = Database::new().expect("Database::new() crashed");
-
-    // println!("{:?}", database);
+    let database = Database::new().expect("Database::new() failed");
 }
 
-struct Database(HashMap<String, String>);
+struct Database(PathBuf, HashMap<String, String>);
 
 impl Database {
     fn new() -> Result<Database, Error> {
+        let mut path: PathBuf = PathBuf::from(r"C:\Users\Catherine\Documents\Projects\rust-intro\kv.db");
         let mut map = HashMap::new();
         let contents = fs::read_to_string("kv.db")?;
         for line in contents.lines() {
             let (key, value) = line.split_once('\t').expect("Corrupt database");
-            map.insert(key.to_string(), value.to_string());
+            map.insert(key.to_owned(), value.to_owned());
         }
 
-        Ok(Database(map))
+        Ok(Database(path, map))
     }
 }
